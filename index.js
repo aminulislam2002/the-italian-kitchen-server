f02e49457cf7cbaf7fb2802deb33aa993d09fa71
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-01 15:20:14 -0700

    chefs data running to server

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 5000;
 
+const chefs = require("./data/chefs.json");
+
+app.use(cors());
+
 app.get("/", (req, res) => {
   res.send("server is running!");
+});
+
+app.get("/chefs", (req, res) => {
+  res.send(chefs);
 });
 
 app.listen(port, () => {
