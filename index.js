156751ad239b0ae77a7915d22a7fae71f269901f
aminulislam2002 <aminulislamemon2002@gmail.com>
2023-05-02 03:32:21 -0700

    create single chef recipes url

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const port = 5000;
 
 const chefs = require("./data/chefs.json");
 
+const recipes = require("./data/recipes.json");
+
 app.use(cors());
 
 app.get("/", (req, res) => {
@@ -13,6 +15,22 @@ app.get("/", (req, res) => {
 
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
+});
+
+app.get("/recipes/:id", (req, res) => {
+  const SelectedId = req.params.id;
+  const selectedRecipes = recipes.filter((r) => parseInt(r.id) === parseInt(SelectedId));
+  res.send(selectedRecipes);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const SelectedId = req.params.id;
+  const selectedChef = chefs.find((c) => parseInt(c.id) === parseInt(SelectedId));
+  res.send(selectedChef);
 });
 
 app.listen(port, () => {
